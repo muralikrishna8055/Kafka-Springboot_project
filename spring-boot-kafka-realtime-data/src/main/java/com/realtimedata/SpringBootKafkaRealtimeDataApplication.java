9f85ef221ec89af7d449f317d76de20fdd759985
Murali Krishna <mailstomuralikrishna@gmail.com>
2023-10-18 21:32:37 +0530

    Producer files added

---
+++ spring-boot-kafka-realtime-data/src/main/java/com/realtimedata/SpringBootKafkaRealtimeDataApplication.java
@@ -0,0 +1,13 @@
+package com.realtimedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaRealtimeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaRealtimeDataApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-realtime-data/src/test/java/com/realtimedata/SpringBootKafkaRealtimeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaRealtimeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
