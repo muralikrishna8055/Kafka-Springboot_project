5446f780f2c2571d6bacd1d017357ad7b8e5427d
Murali Krishna <mailstomuralikrishna@gmail.com>
2023-10-19 19:24:27 +0530

    consumer initial files added

---
+++ kafka-conusumer/src/main/java/com/kafkaconsumer/KafkaConusumerApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConusumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConusumerApplication.class, args);
+	}
+
+}
