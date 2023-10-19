fb77203fcef74a9eeaece1da2c2679fc7bf7a087
Murali Krishna <mailstomuralikrishna@gmail.com>
2023-10-19 10:31:58 +0530

    kafka topic and producer configured

--- spring-boot-kafka-realtime-data/src/main/java/com/realtimedata/SpringBootKafkaRealtimeDataApplication.java
+++ spring-boot-kafka-realtime-data/src/main/java/com/realtimedata/SpringBootKafkaRealtimeDataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootKafkaRealtimeDataApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootKafkaRealtimeDataApplication.class, args);
 	}
 
